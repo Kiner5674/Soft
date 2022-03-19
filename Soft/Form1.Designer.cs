3467dea95b7b3cd376272f3590caea321544e2a3
Kiner5674 <hello1010@outlook.jp>
2022-03-19 13:13:13 +0900

    add button_Click Event

--- Soft/Form1.Designer.cs
+++ Soft/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Temp_Delete = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Temp_Delete
+            // 
+            this.Temp_Delete.Location = new System.Drawing.Point(12, 12);
+            this.Temp_Delete.Name = "Temp_Delete";
+            this.Temp_Delete.Size = new System.Drawing.Size(132, 23);
+            this.Temp_Delete.TabIndex = 0;
+            this.Temp_Delete.Text = "Temp Delete";
+            this.Temp_Delete.UseVisualStyleBackColor = true;
+            this.Temp_Delete.Click += new System.EventHandler(this.Temp_Delete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.Temp_Delete);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Temp_Delete;
     }
 }
 
--- Soft/Form1.cs
+++ Soft/Form1.cs
@@ -16,5 +16,10 @@ namespace Soft
         {
             InitializeComponent();
         }
+
+        private void Temp_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
