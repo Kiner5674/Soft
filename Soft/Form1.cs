96882bdae10fd120729e0faab9ae2f287d2d9385
Kiner5674 <hello1010@outlook.jp>
2022-03-20 09:54:13 +0900

    Temp Delete Process Complete

--- Soft/Form1.cs
+++ Soft/Form1.cs
@@ -14,13 +14,24 @@ namespace Soft
 
         private void Temp_Delete_Click(object sender, EventArgs e)
         {
-            try 
+            try
             {
-                System.IO.Directory.Delete(@"C:\Windows\Temp",true);
+                System.IO.Directory.Delete(@"C:\Windows\Temp", true);
             }
-            catch (System.UnauthorizedAccessException)
+            catch (System.UnauthorizedAccessException) //管理者権限ないとき
             {
-
+                MessageBox.Show(
+                    "アクセス権限ないよ。(UnauthorizedAccessException)\n\n対処法:管理者権限で起動する。",
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (System.IO.IOException) //ファイルがロックされているとき(一部)
+            {
+                MessageBox.Show("I/Oエラーが起きたよ(IOException)\n\n対処法:しばらく待つかPCを再起動してみましょう。", 
+                    "エラー", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
             }
         }
 
